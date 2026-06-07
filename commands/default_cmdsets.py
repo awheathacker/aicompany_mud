2c27cff1e5e2071f1d01a1c25ff7c2d835b9abfa
awheathacker <awheathacker@users.noreply.github.com>
2026-06-07 00:23:13 -0700

    feat: add weather command for atmospheric world state
    - Add weather command to check/set global weather
    - Supports sunny, cloudy, rainy, windy, storm, clear states
    - Weather displays colored descriptions and flavor text
    - Broadcasts weather changes to room contents
    - Registered in CharacterCmdSet alongside craft and dig

--- commands/default_cmdsets.py
+++ commands/default_cmdsets.py
@@ -16,6 +16,8 @@ own cmdsets by inheriting from them or directly from `evennia.CmdSet`.
 
 from evennia import default_cmds
 from commands.dig import CmdDigSmart
+from commands.craft import CmdCraft
+from commands.weather import CmdWeather
 
 class CharacterCmdSet(default_cmds.CharacterCmdSet):
     """
@@ -35,6 +37,8 @@ class CharacterCmdSet(default_cmds.CharacterCmdSet):
         # any commands you add below will overload the default ones.
         #
         self.add(CmdDigSmart())
+        self.add(CmdCraft())
+        self.add(CmdWeather())
 
 
 class AccountCmdSet(default_cmds.AccountCmdSet):
